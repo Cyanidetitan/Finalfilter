bc6e07d5000d188fdba793d06959e258c4bac8ab
staneswilson <82250240+staneswilson@users.noreply.github.com>
2021-09-29 14:49:41 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -90,7 +90,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
+            InlineKeyboardButton('Search again', url='https://t.me/MoveWilla1,
             InlineKeyboardButton('⚡Channels⚡', url='https://t.me/MoveWilla_Links')
         ]
         ]
