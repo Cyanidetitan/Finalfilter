bf7d07828766ef0fe8403e25808f1a32b9a1446c
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-10-20 13:20:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ["5894098166"].split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ[5894098166].split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ["-1002100546607"].split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', "5894098166").split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
