69ef3895e0bc8d72ad78dbfaf026b37097ea6f83
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-10-28 22:22:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'library')
 # Messages
 default_start_msg = """
 **Hi, I'm Pavillion**
-Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name of file to search.
+Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name of Anime to search.
 """
 START_MSG = environ.get('START_MSG', default_start_msg)
 
